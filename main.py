fff781498023e8d36314bf9978be73869275d176
Varvara Chornomorets <vchornomorets@kse.org.ua>
2023-01-11 13:54:14 +0200

    ball changes direction after intersection with walls or y-coordinate of Paddle

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
-from cls import Vector, Paddle
+from cls import Vector, Paddle, Ball
 import pgzrun
 
-WIDTH = 800
+WIDTH = 600
 HEIGHT = 600
 
-some_position = Vector(700, 500)
-platform = Paddle(some_position)
+platform_start_position = Vector(700, 500)
+ball_start_pos = Vector(17, 20)
+platform = Paddle(platform_start_position)
+my_ball = Ball(ball_start_pos)
 
 
 def draw():
     screen.clear()
     platform.draw(screen)
+    my_ball.draw(screen)
 
 
 def update(dt):
     platform.update(dt)
+    my_ball.update(dt)
 
 
 def on_mouse_move(pos):
