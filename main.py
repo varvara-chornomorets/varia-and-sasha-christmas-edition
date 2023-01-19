01fa44d3e7d666cc746b03f6848e2657062453b5
Varvara Chornomorets <vchornomorets@kse.org.ua>
2023-01-19 11:35:50 +0200

    changed ball start position back to (17, 20)

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ WIDTH = 600
 HEIGHT = 600
 
 platform_start_position = Vector(700, 500)
-ball_start_pos = Vector(150,250)
+ball_start_pos = Vector(17,20)
 platform = Paddle(platform_start_position)
 my_ball = Ball(ball_start_pos)
 number_of_lives = 3
